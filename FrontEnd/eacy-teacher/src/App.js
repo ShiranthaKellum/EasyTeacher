8e7a5f21399a403922ffd4ca2bf94bc8528fe2ba
Shirantha Kellum <skellumjayasooriya@gmail.com>
2022-09-07 09:09:10 +0530

    Overall Dashboard page.

--- FrontEnd/eacy-teacher/src/App.js
+++ FrontEnd/eacy-teacher/src/App.js
@@ -1,10 +1,12 @@
 import LogIn from "./Components/LogIn/logIn";
 import SignUp from "./Components/SignUp/signUp";
+import Dashboard from "./Pages/TeacherDashBoard/teacherDashBoard";
 
 function App() {
   return (
     // <LogIn/>
-    <SignUp/>
+    // <SignUp/>
+    <Dashboard/>
 
   )
 }
